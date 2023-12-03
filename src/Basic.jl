bd508d79df73ff034a61a452de3c15e4bb767cf0
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-03 16:44:59 -0500

    src: Add files

---
+++ src/Basic.jl
@@ -0,0 +1,42 @@
+module Basic
+
+export Grid, GridF
+
+struct Grid
+
+    xmin::Float64
+    xmax::Float64
+    nx::Int64
+    x::Vector{Float64}
+    dx::Float64
+
+    function Grid(nx0, bbox)
+        nx = nx0
+        xmin = bbox[1]
+        xmax = bbox[2]
+        dx = (xmax - xmin) / (nx - 1)
+        x = LinRange(xmin, xmax, nx)
+        new(xmin, xmax, nx, x, dx)
+    end
+
+end
+
+struct GridF
+
+    ndim::Int64
+    gird::Grid
+    u::Array{Array{Float64,1},1}
+    dxu::Array{Array{Float64,1},1}
+
+    function GridF(ndim, grid)
+
+        nx = grid.nx
+        u = Array{Array{Float64,1},1}(undef, ndim)
+        dxu = Array{Array{Float64,1},1}(undef, ndim)
+
+
+    end
+
+end
+
+end # module Basic
