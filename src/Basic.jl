65c0e2e95c0858e5195a1e72c3424fe083f8fd32
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-03 22:37:48 -0500

    src: AutoFormat Basic and Physical

--- src/Basic.jl
+++ src/Basic.jl
@@ -4,42 +4,42 @@ export Grid, GridF
 
 struct Grid
 
-    nx::Int64
-    xmin::Float64
-    xmax::Float64
-    dx::Float64
-    x::Vector{Float64}
+  nx::Int64
+  xmin::Float64
+  xmax::Float64
+  dx::Float64
+  x::Vector{Float64}
 
-    function Grid(nx0, bbox)
-        nx = nx0
-        xmin = bbox[1]
-        xmax = bbox[2]
-        dx = (xmax - xmin) / (nx - 1)
-        x = LinRange(xmin, xmax, nx)
-        new(nx, xmin, xmax, dx, x)
-    end
+  function Grid(nx0, bbox)
+    nx = nx0
+    xmin = bbox[1]
+    xmax = bbox[2]
+    dx = (xmax - xmin) / (nx - 1)
+    x = LinRange(xmin, xmax, nx)
+    new(nx, xmin, xmax, dx, x)
+  end
 
 end
 
 struct GridF
 
-    nd::Int64
-    grid::Grid
-    u::Array{Array{Float64,1},1}
-    dxu::Array{Array{Float64,1},1}
+  nd::Int64
+  grid::Grid
+  u::Array{Array{Float64,1},1}
+  dxu::Array{Array{Float64,1},1}
 
-    function GridF(nd, grid)
+  function GridF(nd, grid)
 
-        nx = grid.nx
-        u = Array{Array{Float64,1},1}(undef, nd)
-        dxu = Array{Array{Float64,1},1}(undef, nd)
-        for i = 1:nd
-            u[i] = zeros(Float64, nx)
-            dxu[i] = zeros(Float64, nx)
-        end
-        new(nd, grid, u, dxu)
-
+    nx = grid.nx
+    u = Array{Array{Float64,1},1}(undef, nd)
+    dxu = Array{Array{Float64,1},1}(undef, nd)
+    for i = 1:nd
+      u[i] = zeros(Float64, nx)
+      dxu[i] = zeros(Float64, nx)
     end
+    new(nd, grid, u, dxu)
+
+  end
 
 end
 
--- src/Physical.jl
+++ src/Physical.jl
@@ -2,21 +2,21 @@ module Physical
 
 function InitialData!(gfs)
 
-    println("Setting initial data ...")
+  println("Setting initial data ...")
 
-    nx = gfs.grid.nx
-    u = gfs.u[1]
-    rho = gfs.u[2]
-    x = gfs.grid.x
+  nx = gfs.grid.nx
+  u = gfs.u[1]
+  rho = gfs.u[2]
+  x = gfs.grid.x
 
-    amp = 1.0
-    sig = 0.1
-    x0 = 0.0
+  amp = 1.0
+  sig = 0.1
+  x0 = 0.0
 
-    for i = 1:nx
-        u[i] = amp * exp(-((x[i] - x0) / sig)^2)
-        rho[i] = 0.0
-    end
+  for i = 1:nx
+    u[i] = amp * exp(-((x[i] - x0) / sig)^2)
+    rho[i] = 0.0
+  end
 
 end
 
