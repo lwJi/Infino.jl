a89da03471d9ea6f492e24732aa30005abe36d2e
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-04 15:31:45 -0500

    src: Output Pi also

--- src/Subcycling.jl
+++ src/Subcycling.jl
@@ -18,7 +18,7 @@ function main()
   println("===================================================================")
 
   nx = 101
-  bbox = [-1.0, 1.0]
+  bbox = [-4.0, 4.0]
   grid = Basic.Grid(nx, bbox)
 
   gfs = Basic.GridF(2, grid)
@@ -30,6 +30,12 @@ function main()
   println("  dt = ", gfs.grid.dt)
   println()
 
+  itlast = 500
+  out_every = 20
+  yrange = (0, 1)
+  a_psi = Animation()
+  a_Pi = Animation()
+
   ###############
   # Intial Data #
   ###############
@@ -39,17 +45,15 @@ function main()
   @printf("Simulation time: %.4f, iteration %d. |psi| = %.4f\n",
           gfs.grid.time, 0, norm(gfs.u[1]))
 
-  a_u = Animation()
-  plt_u = plot(gfs.grid.x, gfs.u[1], labal="u")
-  # plt_u = scatter!(gfs.grid.x, gfs.u[1])
-  frame(a_u, plt_u)
+  plt_psi = plot(gfs.grid.x, gfs.u[1], ylim=(-1,1), labal="psi")
+  # plt_psi = scatter!(gfs.grid.x, gfs.u[1])
+  plt_Pi = plot(gfs.grid.x, gfs.u[2], ylim=(-4,4), labal="Pi")
+  frame(a_psi, plt_psi)
+  frame(a_Pi, plt_Pi)
 
   ##########
   # Evolve #
   ##########
-  itlast = 20
-  out_every = 20
-
   println("Start evolution...")
   for i = 1:itlast
     ODESolver.rk4!(Physical.WaveRHS!, gfs)
@@ -57,14 +61,17 @@ function main()
             gfs.grid.time, i, norm(gfs.u[1]))
 
     if (mod(i, out_every) == 0)
-      plt_u = plot(gfs.grid.x, gfs.u[1], labal="u")
-      # plt_u = scatter!(gfs.grid.x, gfs.u[1])
-      frame(a_u, plt_u)
+      plt_psi = plot(gfs.grid.x, gfs.u[1], ylim=(-1,1), labal="psi")
+      # plt_psi = scatter!(gfs.grid.x, gfs.u[1])
+      plt_Pi = plot(gfs.grid.x, gfs.u[2], ylim=(-4,4), labal="Pi")
+      frame(a_psi, plt_psi)
+      frame(a_Pi, plt_Pi)
     end
   end
 
   # output
-  gif(a_u, "u.gif")
+  gif(a_psi, "psi.gif")
+  gif(a_Pi, "Pi.gif")
 
   ########
   # Exit #
