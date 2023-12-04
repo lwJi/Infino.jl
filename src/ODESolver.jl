dd84ada5b50c0f9a8058b400b4ac98524f288d35
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-04 11:47:02 -0500

    src: Add RK4

--- src/ODESolver.jl
+++ src/ODESolver.jl
@@ -10,7 +10,7 @@ function euler!(f::Function, gfs)
 
   u = gfs.u
   u_p = gfs.u_p
-  r = gfs.r
+  r = gfs.rhs
   grid = gfs.grid
   t = grid.time
   dt = grid.dt
@@ -18,14 +18,46 @@ function euler!(f::Function, gfs)
   x = grid.x
 
   u_p = u
-  gfs.grid.time = t
-  f(gfs.grid, r, u)
+  grid.time = t
+  f(grid, r, u)
   @. u += r * dt
-  gfs.grid.time = t + dt
+  grid.time = t + dt
 
 end
 
-function rk4!()
+function rk4!(f::Function, gfs)
+
+  u = gfs.u
+  u_p = gfs.u_p
+  r = gfs.rhs
+  w = gfs.w
+  grid = gfs.grid
+  t = grid.time
+  dt = grid.dt
+  dx = grid.dx
+  x = grid.x
+
+  u_p = u
+  grid.time = t
+  f(grid, r, u)
+  @. u += r * (dt/6)
+
+  @. w = u_p + r * (dt/2)
+  grid.time = t + 0.5 * dt
+  f(grid, r, w)
+  @. u += r * (dt/3)
+
+  @. w = u_p + r * (dt/2)
+  grid.time = t + 0.5 * dt
+  f(grid, r, w)
+  @. u += r * (dt/3)
+
+  @. w = u_p + r * dt
+  grid.time = t + dt
+  f(grid, r, w)
+  @. u += r * (dt/6)
+  grid.time = t + dt
+
 end
 
 end
