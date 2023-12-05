5b3b65c65d3cd77be1e01c5dff92a2eff2d8d9c1
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-05 15:32:16 -0500

    src: Add LevelFunction to Basic

--- src/Basic.jl
+++ src/Basic.jl
@@ -1,6 +1,6 @@
 module Basic
 
-export Level, Grid, GridF
+export Level, LevelFunction, Grid, GridFunction
 
 struct Level
 
@@ -19,12 +19,38 @@ struct Level
 
 end
 
+struct LevelFunction
+  nd ::Int64
+  lev::Level
+  x  ::Array{Float64,1}
+  u  ::Array{Array{Float64,1},1}
+  u_p::Array{Array{Float64,1},1}
+  rhs::Array{Array{Float64,1},1}
+  w  ::Array{Array{Float64,1},1}
+
+  function LevelFunction(nd, lev)
+    x   = LinRange(lev.xmin, lev.xmax, lev.nx)
+    u   = Array{Array{Float64,1},1}(undef, nd)
+    u_p = Array{Array{Float64,1},1}(undef, nd)
+    rhs = Array{Array{Float64,1},1}(undef, nd)
+    w   = Array{Array{Float64,1},1}(undef, nd)
+
+    nx = lev.nx
+    for i = 1:nd
+      u[i]   = zeros(Float64, nx)
+      u_p[i] = zeros(Float64, nx)
+      rhs[i] = zeros(Float64, nx)
+      w[i]   = zeros(Float64, nx)
+    end
+    new(nd, lev, x, u, u_p, rhs, w)
+  end
+end
+
 mutable struct Grid
 
   levs::Vector{Level}
-  xs::Vector{Vector{Float64}}
   time::Float64
-  dt::Float64
+  dt  ::Float64
 
   function Grid(nx1, bnds::Vector{Vector{Float64}}, cfl=0.4, t=0.0)
     dx1 = (bnds[1][2] - bnds[1][1]) / (nx1 - 1)
@@ -32,18 +58,14 @@ mutable struct Grid
     lev1 = Level(nx1, bnds[1], dt1)
 
     levs = Vector{Level}([lev1])
-    xs = Vector{Vector{Float64}}([LinRange(lev1.xmin, lev1.xmax, lev1.nx)])
     for i = 2:length(bnds)
       dx = dx1 / 2^(i-1)
-      dt = cfl * dx
       levl = levs[i-1]
       xnew = LinRange(levl.xmin, levl.xmax, (levl.nx - 1)*2 + 1)
       xmin = xnew[argmin(abs.(xnew .- bnds[i][1]))]
       ncell = floor(Int, (bnds[i][2] - xmin) / dx)
       bnd = [xmin, xmin + ncell * dx]
-      lev = Level(ncell+1, bnd, dt)
-      push!(levs, lev)
-      push!(xs, LinRange(lev.xmin, lev.xmax, lev.nx))
+      push!(levs, Level(ncell+1, bnd, cfl * dx))
     end
 
     println("Grid Structure:")
@@ -56,42 +78,20 @@ mutable struct Grid
       println("  dt   = ", levs[i].dt)
     end
 
-    new(levs, xs, t, dt1)
+    new(levs, t, dt1)
   end
 
 end
 
-struct GridF
+struct GridFunction
 
-  nd::Int64
-  grid::Grid
-  u  ::Array{Array{Array{Float64,1},1},1}
-  u_p::Array{Array{Array{Float64,1},1},1}
-  rhs::Array{Array{Array{Float64,1},1},1}
-  w  ::Array{Array{Array{Float64,1},1},1}
+  nd   ::Int64
+  grid ::Grid
+  levfs::Vector{LevelFunction}
 
-  function GridF(nd, grid)
-    nlev = length(grid.levs)
-    u   = Array{Array{Array{Float64,1},1},1}(undef, nd)
-    u_p = Array{Array{Array{Float64,1},1},1}(undef, nd)
-    rhs = Array{Array{Array{Float64,1},1},1}(undef, nd)
-    w   = Array{Array{Array{Float64,1},1},1}(undef, nd)
-
-    for i = 1:nd
-      u[i]   = Array{Array{Float64,1},1}(undef, nlev)
-      u_p[i] = Array{Array{Float64,1},1}(undef, nlev)
-      rhs[i] = Array{Array{Float64,1},1}(undef, nlev)
-      w[i]   = Array{Array{Float64,1},1}(undef, nlev)
-
-      for l = 1:nlev
-        nx = grid.levs[l].nx
-        u[i][l]   = zeros(Float64, nx)
-        u_p[i][l] = zeros(Float64, nx)
-        rhs[i][l] = zeros(Float64, nx)
-        w[i][l]   = zeros(Float64, nx)
-      end
-    end
-    new(nd, grid, u, u_p, rhs, w)
+  function GridFunction(nd, grid)
+    levfs = [LevelFunction(nd, lev) for lev in grid.lev]
+    new(nd, grid, levfs)
   end
 
 end
--- src/test/Test.jl
+++ src/test/Test.jl
@@ -9,11 +9,14 @@ function main()
   grid = Basic.Grid(nx, bnds)
 
   ax = Animation()
-  plt = plot(grid.xs[1], zeros(Float64, grid.levs[1].nx),
+  x1 = LinRange(grid.levs[1].xmin, grid.levs[1].xmax, grid.levs[1].nx)
+  x2 = LinRange(grid.levs[2].xmin, grid.levs[2].xmax, grid.levs[2].nx)
+  x3 = LinRange(grid.levs[3].xmin, grid.levs[3].xmax, grid.levs[3].nx)
+  plt = plot(x1, zeros(Float64, grid.levs[1].nx),
              xlim=(0.3, 1.7), ylim=(-0.1, 0.6))
-  plt = scatter!(grid.xs[1], zeros(Float64, grid.levs[1].nx))
-  plt = scatter!(grid.xs[2], zeros(Float64, grid.levs[2].nx) .+ 0.1)
-  plt = scatter!(grid.xs[3], zeros(Float64, grid.levs[3].nx) .+ 0.2)
+  plt = scatter!(x1, zeros(Float64, grid.levs[1].nx))
+  plt = scatter!(x2, zeros(Float64, grid.levs[2].nx) .+ 0.1)
+  plt = scatter!(x3, zeros(Float64, grid.levs[3].nx) .+ 0.2)
   frame(ax, plt)
 
   gif(ax, "x.gif")
