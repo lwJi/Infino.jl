1fe686e7f01f51af06daaec12bd734824bd696d4
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-07 11:58:53 -0500

    src: Add map between neighbor levels

--- src/Basic.jl
+++ src/Basic.jl
@@ -8,16 +8,18 @@ mutable struct Level
   ngh ::Int64  # num of ghost points
   nbuf::Int64  # num of buffer points
   nxa ::Int64  # num of all grid points
-  ibox::Array{Int64,1}  # index of current lev boundary in its parent grid array
   xbox::Array{Float64,1}  # size computational domain (interior)
   dx  ::Float64
   dt  ::Float64
   time::Float64
+  # dimension nxa
+  ir2c::Array{Int64,1}  # map between indexes of current and its parent level
+  align::Array{Bool,1}  # if grid align with coarse grid
 
-  function Level(nx, ngh, nbuf, ibox, xbox, dt, t)
+  function Level(nx, ngh, nbuf, xbox, dt, t, ir2c, align)
     nxa = nx + 2*nbuf
     dx = (xbox[2] - xbox[1]) / (nx - 1)
-    new(nx, ngh, nbuf, nxa, ibox, xbox, dx, dt, t)
+    new(nx, ngh, nbuf, nxa, xbox, dx, dt, t, ir2c, align)
   end
 
 end
@@ -61,7 +63,7 @@ mutable struct Grid
   function Grid(nx1, xboxs::Vector{Vector{Float64}}, ngh, nbuf, cfl=0.4, t=0.0)
     dx1 = (xboxs[1][2] - xboxs[1][1]) / (nx1 - 1)
     dt1 = cfl * dx1
-    lev1 = Level(nx1, ngh, nbuf, [1, nx1], xboxs[1], dt1, t)
+    lev1 = Level(nx1, ngh, nbuf, xboxs[1], dt1, t, [], [])
     levs = Vector{Level}([lev1])
     for i = 2:length(xboxs)
       dx = dx1 / 2^(i-1)
@@ -74,10 +76,11 @@ mutable struct Grid
       # xl = LinRange(levl.xbox[1], levl.xbox[2], levl.nx)
       # imin = findall(x->abs(x - xboxs[i][1]) <= dx + 1e-12, xl)[1]
       # imax = findall(x->abs(x - xboxs[i][2]) <= dx + 1e-12, xl)[end]
-      ibox = [imin, imax]
       xbox = [xl[imin], xl[imax]]
-      ncell = floor(Int, (xl[imax] - xl[imin]) / dx)
-      push!(levs, Level(ncell+1, ngh, nbuf, ibox, xbox, cfl * dx, t))
+      nx = (imax - imin) + 1  #  (floor(Int, (xl[imax] - xl[imin]) / dx)) + 1
+      ir2c = div.(((imin-nbuf:imax+nbuf) .+ 1), 2)
+      align = mod.(((imin-nbuf:imax+nbuf) .+ 1), 2) .== 0
+      push!(levs, Level(nx, ngh, nbuf, xbox, cfl * dx, t, ir2c, align))
     end
     println("Grid Structure:")
     for i = 1:length(levs)
@@ -85,10 +88,19 @@ mutable struct Grid
       println("  nx   = ", levs[i].nx)
       println("  ngh  = ", levs[i].ngh)
       println("  nbuf = ", levs[i].nbuf)
-      println("  ibox = ", levs[i].ibox)
+      if (length(levs[i].ir2c) == levs[i].nxa)
+        println("  ibox = ",
+                [levs[i].ir2c[1+levs[i].nbuf],
+                 levs[i].ir2c[levs[i].nxa-levs[i].nbuf]],
+                ", ",
+                [levs[i].align[1+levs[i].nbuf],
+                 levs[i].align[levs[i].nxa-levs[i].nbuf]])
+      end
       println("  xbox = ", levs[i].xbox)
       println("  dx   = ", levs[i].dx)
       println("  dt   = ", levs[i].dt)
+      # println("  ir2c = ", levs[i].ir2c)
+      # println("  align= ", levs[i].align)
     end
     new(levs, dt1, t)
   end
