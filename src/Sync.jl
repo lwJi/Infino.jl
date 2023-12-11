b1941824153f40c664db667a729ab0ae9789efc9
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-10 20:51:43 -0500

    src: Beautify Sync and add data

--- src/Sync.jl
+++ src/Sync.jl
@@ -1,36 +1,38 @@
 module Sync
 
 function Prolongation(gfs, l, interp_in_time::Bool)
-  levl  = gfs.grid.levs[l]
-  nxa   = levl.nxa
-  nbuf  = levl.nbuf
-  if2c  = levl.if2c
-  aligned = levl.aligned
-  levsfs = gfs.levs
+  nxa     = gfs.grid.levs[l].nxa
+  nbuf    = gfs.grid.levs[l].nbuf
+  if2c    = gfs.grid.levs[l].if2c
+  aligned = gfs.grid.levs[l].aligned
+  levsfs  = gfs.levs
 
   for v in 1:gfs.nd
-    u    = levsfs[l].u[v]
+    uf   = levsfs[l].u[v]
     uc_p = levsfs[l-1].u_p[v]
     if (interp_in_time)
       uc = levsfs[l-1].u[v]
-      for il in 1:nbuf
-        ir = nxa - il + 1
-        icl = if2c[il]
-        icr = if2c[ir]
-        u[il] = ((aligned[il])
-                 ? (uc[icl] + uc_p[icl]) * 0.5
-                 : (uc[icl] + uc[icl+1] + uc_p[icl] + uc_p[icl+1]) * 0.25)
-        u[ir] = ((aligned[ir])
-                 ? (uc[icr] + uc_p[icr]) * 0.5
-                 : (uc[icr] + uc[icr+1] + uc_p[icr] + uc_p[icr+1]) * 0.25)
+      for f in 1:nbuf
+        c = if2c[f]
+        uf[f] = ((aligned[f])
+                 ? (uc[c] + uc_p[c]) * 0.5
+                 : (uc[c] + uc[c+1] + uc_p[c] + uc_p[c+1]) * 0.25)
+      end
+      for f in nxa:-1:nxa-nbuf+1
+        c = if2c[f]
+        uf[f] = ((aligned[f])
+                 ? (uc[c] + uc_p[c]) * 0.5
+                 : (uc[c] + uc[c+1] + uc_p[c] + uc_p[c+1]) * 0.25)
       end
     else
-      for il in 1:nbuf
-        ir = nxa - il + 1
-        icl = if2c[il]
-        icr = if2c[ir]
-        u[il] = ((aligned[il]) ? uc_p[icl] : (uc_p[icl] + uc_p[icl+1]) * 0.5)
-        u[ir] = ((aligned[ir]) ? uc_p[icr] : (uc_p[icr] + uc_p[icr+1]) * 0.5)
+      # here we assume that we always march coarse level first: l in 1:lmax
+      for f in 1:nbuf
+        c = if2c[f]
+        uf[f] = ((aligned[f]) ? uc_p[c] : (uc_p[c] + uc_p[c+1]) * 0.5)
+      end
+      for f in nxa:-1:nxa-nbuf+1
+        c = if2c[f]
+        uf[f] = ((aligned[f]) ? uc_p[c] : (uc_p[c] + uc_p[c+1]) * 0.5)
       end
     end
   end
