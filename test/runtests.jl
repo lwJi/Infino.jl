6652a54c8cf105705ff22bb09066d435b46d0e54
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-14 13:02:23 -0500

    test: Add unit test for ODESolver

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,12 @@ using SafeTestsets
 @time begin
     @safetestset "Basic" begin
         include("unit/basic.jl")
+        include("unit/odesolver.jl")
+    end
+    @safetestset "Derivs" begin
         include("unit/derivs.jl")
+    end
+    @safetestset "ODESolver" begin
+        include("unit/odesolver.jl")
     end
 end
---
+++ test/unit/odesolver.jl
@@ -0,0 +1,46 @@
+using Infino
+
+function example_ode!(lev, r, u)
+    t = lev.time
+    y = u[1]
+    y_rhs = r[1]
+    @. y_rhs = -0.5 * y + 0.5 * (98 + t) / (100 + t)^2
+end
+
+function analytical_solution(t)
+    return (t + 100)^(-1) + 2 * exp(-0.5 * t)
+end
+
+@testset "Euler Method Tests" begin
+    g = Infino.Basic.Grid(2, [[-1.0, 1.0]], 0, 0, 0.005)
+    gfs = Infino.Basic.GridFunction(1, g)
+    # initial data
+    u = gfs.levs[1].u[1]
+    x = gfs.levs[1].x
+    @. u = analytical_solution.(0)
+    # evolution
+    for i = 1:4
+        Infino.ODESolver.euler!(example_ode!, gfs.levs[1])
+    end
+    t = g.levs[1].time
+    error = (gfs.levs[1].u[1][1] - analytical_solution(t)) / analytical_solution(t)
+    # println(error)
+    @test isapprox(error, 0; atol = 1e-4)
+end
+
+@testset "RK4 Method Tests" begin
+    g = Infino.Basic.Grid(2, [[-1.0, 1.0]], 0, 0, 0.25)
+    gfs = Infino.Basic.GridFunction(1, g)
+    # initial data
+    u = gfs.levs[1].u[1]
+    x = gfs.levs[1].x
+    @. u = analytical_solution.(0)
+    # evolution
+    for i = 1:4
+        Infino.ODESolver.rk4!(example_ode!, gfs.levs[1])
+    end
+    t = g.levs[1].time
+    error = (gfs.levs[1].u[1][1] - analytical_solution(t)) / analytical_solution(t)
+    # println(error)
+    @test isapprox(error, 0; atol = 1e-4)
+end
