304a0c21f318bf5b3cc6aec2446a17f4fd43496b
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-14 17:05:52 -0500

    test: Add unit test of scalarwave using AMR

--- test/unit/scalarwave.jl
+++ test/unit/scalarwave.jl
@@ -37,3 +37,45 @@ end
         rtol = 1e-5,
     )
 end
+
+@testset "Scalar Wave Evolution on 3 levels Grid" begin
+    g = Infino.Basic.Grid(
+        100,
+        [[-0.5, 0.5 - 0.01], [-0.25, 0.25 - 0.005], [-0.125, 0.125 - 0.0025]],
+        2,
+        2;
+        cfl = 0.25,
+        verbose = false,
+    )
+    gfs = Infino.Basic.GridFunction(2, g)
+    # initial data
+    for l = 1:length(gfs.levs)
+        psi = gfs.levs[l].u[1]
+        Pi = gfs.levs[l].u[2]
+        x = gfs.levs[l].x
+        @. psi = analytical_psi(0, x)
+        @. Pi = analytical_Pi(0, x)
+    end
+    Infino.Boundary.ApplyPeriodicBoundaryCondition!(gfs)
+    # evolution
+    for i = 1:4
+        Infino.ODESolver.Evolve!(Infino.Physical.WaveRHS!, gfs)
+        Infino.Boundary.ApplyPeriodicBoundaryCondition!(gfs)
+    end
+    for l = 1:length(gfs.levs)
+        t = g.time
+        nxa = g.levs[l].nxa
+        nbuf = g.levs[l].nbuf
+        x = gfs.levs[l].x
+        @test isapprox(
+            gfs.levs[l].u[1][1+nbuf:nxa-nbuf],
+            analytical_psi.(t, x)[1+nbuf:nxa-nbuf];
+            rtol = 1e-3,
+        )
+        @test isapprox(
+            gfs.levs[l].u[2][1+nbuf:nxa-nbuf],
+            analytical_Pi.(t, x)[1+nbuf:nxa-nbuf];
+            rtol = 1e-2,
+        )
+    end
+end
