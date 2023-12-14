f7fa1118f76bd705e9e828adb7b8cb26fd54c299
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-14 15:45:21 -0500

    src: Add Periodic boundary condition

--- src/Boundary.jl
+++ src/Boundary.jl
@@ -1,5 +1,18 @@
 module Boundary
 
-function ApplyPeriodicBoundaryCondition!(gfs) end
+function ApplyPeriodicBoundaryCondition!(gfs)
+    lev1fs = gfs.levs[1]
+    nxa = gfs.grid.levs[1].nxa
+    nbuf = gfs.grid.levs[1].nbuf
+    for v = 1:gfs.nd
+        u = lev1fs.u[v]
+        for i = 1:nbuf
+            u[i] = u[nxa-2*nbuf+i]
+        end
+        for i = nxa:-1:nxa-nbuf+1
+            u[i] = u[2*nbuf-nxa+i]
+        end
+    end
+end
 
 end
