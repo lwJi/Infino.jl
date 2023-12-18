dc927e591a55d6858745d161bfc68323b496690f
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-18 08:04:33 -0500

    src: Beautify InitialData

--- src/InitialData.jl
+++ src/InitialData.jl
@@ -3,6 +3,7 @@ module InitialData
 include("Derivs.jl")
 include("Sync.jl")
 include("ODESolver.jl")
+include("Physical.jl")
 
 function Gaussian!(gfs)
     lmax = length(gfs.levs)
@@ -26,34 +27,27 @@ function Gaussian!(gfs)
 end
 
 function NegativeWaveRHS!(lev, r, u)
-    psi = u[1]
-    Pi = u[2]
-    psi_rhs = r[1]
-    Pi_rhs = r[2]
-    # derivatives
-    ddpsi = zeros(Float64, lev.nxa)
-    Derivs.derivs_2nd!(ddpsi, psi, lev.dx, lev.ngh * 2)
-
-    @. psi_rhs = -Pi
-    @. Pi_rhs = -ddpsi
+    Physical.WaveRHS!(lev, r, u)
+    @. r = -r
 end
 
 function MarchBackwards!(gfs)
     for l = 1:length(gfs.levs)
-        levfs = gfs.levs[l]
+        levf = gfs.levs[l]
 
         if l > 1
             Sync.Prolongation(gfs, l, false)
         end
-        ODESolver.rk4!(NegativeWaveRHS!, levfs)
+        ODESolver.rk4!(NegativeWaveRHS!, levf)
 
-        u = levfs.u
-        u_p = levfs.u_p
-        u_pp = levfs.u_pp
+        u = levf.u
+        u_p = levf.u_p
+        u_pp = levf.u_pp
         @. u_pp = u_p
         @. u_p = u
         @. u = u_pp
-        levfs.lev.time = 0.0
+
+        levf.lev.time = 0.0
     end
     gfs.grid.time = 0.0
 end
