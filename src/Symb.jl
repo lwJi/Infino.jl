6ec4ce4fc2787be11fa384784d431d30f6a8f7ae
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-21 21:08:41 -0500

    src: Update dy in Symb

--- src/Symb.jl
+++ src/Symb.jl
@@ -5,7 +5,7 @@ using Symbolics
 #===============================================================================
 RK's interpolant, termed dense output
 ===============================================================================#
-@variables theta, k[1:4], h, yn
+@variables theta, h, k[1:4], yn
 
 b = [
     theta - 3 // 2 * theta^2 + 2 // 3 * theta^3,
@@ -18,6 +18,7 @@ y = yn + sum([b[i] * k[i] for i = 1:length(b)])
 
 dy_symb(ord) = expand_derivatives((Differential(theta)^ord)(y)) / h^ord
 
-dy(ord) = build_function(dy_symb(ord), [theta, k, h], expression = Val{false})
+# build function
+dy(ord) = build_function(dy_symb(ord), theta, h, k, expression = Val{false})
 
 end
