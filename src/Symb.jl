9c573cbbd60fd1d136a2097182000b5c4ef4dd81
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-23 17:05:23 -0500

    src: Build funciton for y too in Symb

--- src/Symb.jl
+++ src/Symb.jl
@@ -14,11 +14,12 @@ b = [
     -1 // 2 * theta^2 + 2 // 3 * theta^3,
 ]
 
-y = yn + sum([b[i] * k[i] for i = 1:length(b)])
+y_symb = yn + sum([b[i] * k[i] for i = 1:length(b)])
 
-dy_symb(ord) = expand_derivatives((Differential(theta)^ord)(y)) / h^ord
+dy_symb(ord) = expand_derivatives((Differential(theta)^ord)(y_symb)) / h^ord
 
 # build function
+y = build_function(y_symb, theta, yn, k, expression = Val{false})
 dy(ord) = build_function(dy_symb(ord), theta, h, k, expression = Val{false})
 
 end
