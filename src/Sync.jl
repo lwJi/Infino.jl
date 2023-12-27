93cbec0114bec437ba833946b52b17149b3d45dd
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-27 12:54:57 -0500

    src: Fix bug in calc_kfs_from_kcs

--- src/Sync.jl
+++ src/Sync.jl
@@ -57,15 +57,13 @@ end
 
 function calc_kfs_from_kcs(kcs, dtc, interp_in_time::Bool)
     t0_f = interp_in_time ? 0.5 : 0.0
-    thalf_f = interp_in_time ? 0.75 : 0.25
     dtf = 0.5 * dtc
-    d1yc_t0 = DenseOutput.dy1(t0_f, dtc, kcs)
-    d1yc = DenseOutput.dy1(thalf_f, dtc, kcs)
-    d2yc = DenseOutput.dy2(thalf_f, dtc, kcs)
-    d3yc = DenseOutput.dy3(thalf_f, dtc, kcs)
+    d1yc = DenseOutput.dy1(t0_f, dtc, kcs)
+    d2yc = DenseOutput.dy2(t0_f, dtc, kcs)
+    d3yc = DenseOutput.dy3(t0_f, dtc, kcs)
     fyd2yc = 4 * (kcs[3] - kcs[2]) / dtc^3
     return [
-        dtf * d1yc_t0,
+        dtf * d1yc,
         dtf * d1yc + 0.5 * dtf^2 * d2yc + 0.125 * dtf^3 * (d3yc - fyd2yc),
         dtf * d1yc + 0.5 * dtf^2 * d2yc + 0.125 * dtf^3 * (d3yc + fyd2yc),
     ]
